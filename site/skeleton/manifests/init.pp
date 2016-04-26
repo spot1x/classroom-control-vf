f6f91b38dd84c790f13be15e4d12c3421beae51c
spot1x <spot1x@yahoo.com>
2016-04-26 11:48:51 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,6 +1,6 @@
 class skeleton { 
-file { '/etc/skel': 
-   ensure => directory,
+   file { '/etc/skel': 
+      ensure => directory,
       owner => 'root'
       group => 'root'
       mode =>'0755',
