12ebcf2fc6f052104bc76a59a38a61e9c229ddcd
spot1x <spot1x@yahoo.com>
2016-04-27 10:06:42 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,44 +1,39 @@
 #
 class nginx {
 
+$docroot = '/var/www'
+
+
+File {
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    }
+
   # PACKAGE
   package { 'nginx':
     ensure => present,
   }
 
   # DOCROOT
-  file { '/var/www':
+  file { $docroot:
     ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
   }
 
   # WEB PAGE
   file { '/var/www/index.html':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
     source => 'puppet:///modules/nginx/index.html',
   }
 
   # CONFIG FILES
   file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
 
   file { '/etc/nginx/conf.d/default.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
